1ad63bc9e6d33764fa34eecab79e487f52333019
Dave Gurnell <dave@untyped.com>
2013-07-28 17:31:26 +0100

    Fix linecount script so it doesn't count the locations library.

--- linecount.sh
+++ linecount.sh
@@ -2,5 +2,5 @@
 
 # Because the number of lines is what truly matters, right?
 
-echo Java  `find src/main/java  -name \*.java  -exec cat {} \; | grep '[^ ]' | grep -v '^[ ]*[/][/]' | wc -l`
-echo Scala `find src/main/scala -name \*.scala -exec cat {} \; | grep '[^ ]' | grep -v '^[ ]*[/][/]' | wc -l`
+echo Java  `find src/main/java/tweetzor  -name \*.java  -exec cat {} \; | grep '[^ ]' | grep -v '^[ ]*[/][/]' | wc -l`
+echo Scala `find src/main/scala/tweetzor2 -name \*.scala -exec cat {} \; | grep '[^ ]' | grep -v '^[ ]*[/][/]' | wc -l`
